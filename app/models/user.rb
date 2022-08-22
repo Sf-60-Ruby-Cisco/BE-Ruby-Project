86ada8fe4548bbd5f43571ac2fbf05ec824f5f33
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-08-22 17:05:35 +0300

    Email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
   validates_uniqueness_of :username
   has_rich_text :content
   # validate :content
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+   
 
   def login
     @login || self.username || self.email
