5eae846b4db5f7116ec9a9f3876e6a5b118cc72f
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-08-24 10:05:19 +0300

    Fixing sign up inactive path, change all devise page views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
-class HomeController < ApplicationController
-  
-  def index
+class HomeController < ApplicationController  
+  def index   
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' } 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
     get '/users' =>  'devise/registrations#new'
     get '/users/password' => 'devise/passwords#new'
   end
   root 'home#index'  
+
 end
