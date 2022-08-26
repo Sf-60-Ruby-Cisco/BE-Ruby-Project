557f182635eb5a5a8b650259bde30dbb4d467127
Syncwell-257 <presian.kostov@gmail.com>
2022-08-26 14:35:56 +0300

    Linked the users with the cars

---
+++ app/views/cars/_car.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! car, :id, :brand, :model, :engine, :fuel_type, :year, :license_plate, :created_at, :updated_at
+json.url car_url(car, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_08_070012) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_184846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_08_070012) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.string "engine"
+    t.string "fuel_type"
+    t.date "year"
+    t.string "license_plate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
