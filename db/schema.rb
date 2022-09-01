bdb12c097e604ef557462151d9a26821b9e173aa
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-09-01 18:44:48 +0300

    Implementig money-rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_31_101816) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_152948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,11 +67,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_101816) do
 
   create_table "chargings", force: :cascade do |t|
     t.string "brand_station"
-    t.integer "amount", null: false
     t.date "date"
     t.bigint "car_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "amount_cents", default: 0, null: false
+    t.string "amount_currency", default: "USD", null: false
     t.index ["car_id"], name: "index_chargings_on_car_id"
   end
 
