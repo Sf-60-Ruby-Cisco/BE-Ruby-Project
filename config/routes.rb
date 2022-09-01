57022b0b29a8951b5d56a549d2277e62b27d5912
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-09-01 13:57:20 +0300

    Add possibilyty only car picture to be deleted from cars view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :cars
+  resources :cars do
+    member do
+      delete :purge_content
+    end
+  end
   devise_for :users, controllers: { registrations: 'users/registrations' } 
   devise_scope :user do
     get '/users' =>  'devise/registrations#new'
