c87ce21e9707f53e96488e28bef7f47f412dd9f0
Petar <echonykrom@protonmail.com>
2022-09-02 18:43:06 +0300

    update charging_params & update & create

--- app/controllers/chargings_controller.rb
+++ app/controllers/chargings_controller.rb
@@ -5,7 +5,8 @@ class ChargingsController < ApplicationController
 
   # POST /cars/:car_id/chargings
   def create
-    @charging = Charging.new(charging_params.except(:id, :car_id).merge(car_id: @car.id))
+    @charging = Charging.new(charging_params.except(
+            :id, :car_id, :utf8, :_method, :authenticity_token, :commit).merge(car_id: @car.id))
 
     respond_to do |format|
       if @charging.save
@@ -21,7 +22,7 @@ class ChargingsController < ApplicationController
   # PATCH/PUT /cars/:car_id/chargings
   def update
     respond_to do |format|
-      if @charging.update(charging_params.except(:id, :car_id))
+      if @charging.update(charging_params.except(:id, :car_id, :utf8, :_method, :authenticity_token, :commit))
         format.html { redirect_to car_url(@car), status: 303, notice: "Charging was successfully updated." }
         format.json { render :plain => {success:true}.to_json, status: 200, content_type: 'application/json' }
       else
@@ -57,7 +58,7 @@ class ChargingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def charging_params
-      params.permit(:brand_station, :amount, :date, :car_id, :id)
+      params.permit(:utf8, :_method, :authenticity_token, :commit, :brand_station, :amount, :date, :car_id, :id)
     end
 
 end
