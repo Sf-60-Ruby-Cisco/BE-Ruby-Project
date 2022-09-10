834d7b5cf9502f1495254886d8e72d9b4fd88957
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-09-10 14:00:21 +0300

    Make about page description

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,6 @@ class StatisticsController < ApplicationController
 
   def index
     @cars = current_user.cars.order(created_at: :asc)  
-    @total_repairs = current_user.cars.joins(:repairs).group_by_month(:date, format: "%B %Y").sum(:amount_cents)
-  
+    @total_repairs = current_user.cars.joins(:repairs).group_by_month(:date, format: "%B %Y").sum(:amount_cents)  
   end   
 end
