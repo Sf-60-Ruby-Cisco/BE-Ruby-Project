1a34fceeaaa23b3f1fc146b6a7baf706d5784303
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-09-12 16:24:00 +0300

    Fixing chartkick behavior and value displaying

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,6 +2,21 @@ class StatisticsController < ApplicationController
 
   def index
     @cars = current_user.cars.order(created_at: :asc)  
+
     @total_repairs = current_user.cars.joins(:repairs).group_by_month(:date, format: "%B %Y").sum(:amount_cents)  
+
+    @all_cars = []
+    for car in @cars
+      if car.repairs.present?
+        @all_cars.push(car.repairs.group_by_month(:date, format: "%B %Y").sum(:amount_cents))
+      end
+    end
+
+    @license_plates = []
+    for car in @cars
+      if car.repairs.present?
+        @license_plates.push(car.license_plate)
+      end
+    end
   end   
 end
