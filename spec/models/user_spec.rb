408bf45ac3b045f20992fd2d6e48409bda40554d
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-09-19 12:56:11 +0300

    Testing User model, controller and routes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with valid attribues' do
+    it 'is valid with valid atributes' do
+      expect(create(:user)).to be_valid 
+    end
+  end
+
+  context 'with invalid attribues' do  
+    it 'is not valid without password' do
+      expect(build(:user, password: nil)).to_not be_valid  
+    end
+
+    it 'is not valid with difference in confirm password' do
+      expect(build(:user, password_confirmation: '123')).to_not be_valid
+    end
+
+    it 'is not valid with emopty space in username' do
+      expect(build(:user, username: 'Test Test')).to_not be_valid
+    end
+
+    it 'is not valid without email' do
+      expect(build(:user, email: nil)).to_not be_valid   
+    end
+
+    it 'is not valid with invalid email' do
+      expect(build(:user, email: "Invalidmail@")).to_not be_valid      
+    end  
+    
+    it 'rejects names that are too long' do
+      expect(build(:user, username: "a" * 41)).to_not be_valid    
+    end     
+
+  end
 end
