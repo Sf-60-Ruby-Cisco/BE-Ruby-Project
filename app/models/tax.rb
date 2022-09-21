d9fbad45a5a8d419042db45341cf5dd3ee491a2e
Petar <echonykrom@protonmail.com>
2022-09-21 12:04:26 +0300

    Add job, tax mailer, tax mailer views,tax mailer preview

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -2,4 +2,9 @@ class Tax < ApplicationRecord
   belongs_to :car
   monetize :amount_cents, numericality: { in: 0.01..4294967295 }
   validates :description, length: {maximum: 350}
+
+  # Class method
+  def self.due_taxes
+    where("date + 365 = ?", 14.days.from_now.to_date)
+  end
 end
