661e98b7ecffb6e865285d5650e4525e81449b2f
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-09-22 20:24:54 +0300

    Testing cars requests

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -10,5 +10,45 @@ RSpec.describe "Cars", type: :request do
                    { controller: 'cars', action: 'destroy', id: '3' })
     assert_routing({ path: 'cars/1', method: :put},
                    { controller: 'cars', action: 'update', id: '1' })
+    assert_routing({ path: 'cars/1/edit', method: :get},
+                  { controller: 'cars', action: 'edit', id: '1' })
+  end
+
+  let!(:user) { FactoryBot.create(:user)}
+  before(:each) { sign_in user }
+
+  describe 'GET /cars' do
+    let!(:car) { create(:car) }
+    before(:each) { get cars_path }
+    it 'should render the correct template' do
+      expect(response).to have_http_status(200)
+      expect(response).to render_template :index
+    end 
+  end
+
+  describe 'POST /cars' do
+    context 'with valid parameters' do
+      it 'should increase by one' do
+        expect { post cars_path, params: { car: attributes_for(:car) } }
+          .to change { Car.count }.by(1)
+      end
+    end
+    context 'with invalid parameters' do
+      context 'no license plate' do
+        it 'should not save the car' do
+          expect { post cars_path, params: { car: attributes_for(:car, license_plate: nil) } }
+          .to change { Car.count }.by(0)
+        end
+      end
+    end  
+  end
+
+  describe 'DELETE /cars/:id' do
+    context 'with valid id' do
+      let!(:car) { create(:car) }      
+        it 'should delete car' do
+          expect { delete car_path(locale: :en, id: car.id) }.to change { Car.count }.by(-1)
+        end
+    end
   end
 end
