2050eb39e0e0d54c118daaf3e5257b2b8220a299
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-09-27 12:46:07 +0300

    more test on user model and routes of repairs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,8 @@
 
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.email}
-    username {Faker::Internet.username(specifier: 5..10) }
+    email { Faker::Internet.unique.email}
+    username {Faker::Internet.unique.username(specifier: 5..10) }
     password {'123456'}
     password_confirmation {'123456'}
     confirmed_at { Time.now }       
--- spec/requests/repairs_spec.rb
+++ spec/requests/repairs_spec.rb
@@ -2,5 +2,17 @@
 
 require 'rails_helper'
 
-RSpec.describe "Repairs", type: :request do  
+RSpec.describe "Cars", type: :request do  
+  it 'should generate correct routes' do
+    assert_routing({ path: 'cars/1/repairs', method: :post },
+                   { controller: 'repairs', action: 'create', car_id: '1' })
+    assert_routing({ path: 'cars/1/repairs/1', method: :get },
+                   { controller: 'repairs', action: 'show', id: '1', car_id: '1' })
+    assert_routing({ path: '/cars/1/repairs/3', method: :delete},
+                   { controller: 'repairs', action: 'destroy', id: '3', car_id: '1' })
+    assert_routing({ path: '/cars/1/repairs/1', method: :put},
+                   { controller: 'repairs', action: 'update', id: '1', car_id: '1' })
+    assert_routing({ path: '/cars/1/repairs/5/edit', method: :get},
+                  { controller: 'repairs', action: 'edit', id: '5', car_id: '1'  })
+  end
 end
