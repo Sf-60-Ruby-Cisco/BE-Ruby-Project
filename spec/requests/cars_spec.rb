2ec4239c2059e458df0897d720e45281a73c55f2
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2022-09-29 18:05:56 +0300

    Fixing routes in spec model cars

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe "Cars", type: :request do
   
   it 'should generate correct routes' do
     assert_routing({ path: 'cars', method: :post },
-                   { controller: 'cars', action: 'create', locale: 'en' })
+                   { controller: 'cars', action: 'create'})
     assert_routing({ path: 'cars/1', method: :get },
-                   { controller: 'cars', action: 'show', id: '1', locale: 'en'  })
+                   { controller: 'cars', action: 'show', id: '1'})
     assert_routing({ path: 'cars/3', method: :delete},
-                   { controller: 'cars', action: 'destroy', id: '3', locale: 'en'  })
+                   { controller: 'cars', action: 'destroy', id: '3'})
     assert_routing({ path: 'cars/1', method: :put},
-                   { controller: 'cars', action: 'update', id: '1', locale: 'en' })
+                   { controller: 'cars', action: 'update', id: '1' })
     assert_routing({ path: 'cars/1/edit', method: :get},
-                  { controller: 'cars', action: 'edit', id: '1', locale: 'en' })
+                  { controller: 'cars', action: 'edit', id: '1' })
   end
 
   let!(:user) { FactoryBot.create(:user)}
